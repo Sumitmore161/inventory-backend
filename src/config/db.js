0a05ee00a2b8c7b365389c278b372cd56b013822
sumitmore84 <sumit.more@tripfactory.travel>
2026-03-16 16:09:34 +0530

    Added SSL Support for cloud database in dbconfig

--- src/config/db.js
+++ src/config/db.js
@@ -9,6 +9,10 @@ const pool = new Pool({
     database: process.env.DB_NAME ,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: process.env.NODE_ENV === 'production' 
+    ? { rejectUnauthorized: false } 
+    : false,
+
 })
 
 console.log(process.env.DB_USER );
